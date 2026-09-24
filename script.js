cd66ec83f7b1d0afa67e4618ac42009626da9e01
Rohil Masssey <rohilmasssey@gmail.com>
2026-09-24 20:31:44 +0530

    Add new logic

--- script.js
+++ script.js
@@ -72,11 +72,11 @@ async function success(position){
 
     }
 
-    forecastData.daily.temperature_2m_max.forEach((maxtempvalue, index) => {
-        const maxtemp = document.querySelectorAll('.maxtemp');
-        const maxtemprature = Math.floor(maxtempvalue);
-        maxtemp[index].textContent = maxtemprature;
-    });
+    // forecastData.daily.temperature_2m_max.forEach((maxtempvalue, index) => {
+    //     const maxtemp = document.querySelectorAll('.maxtemp');
+    //     const maxtemprature = Math.floor(maxtempvalue);
+    //     maxtemp[index].textContent = maxtemprature;
+    // });
 
     forecastData.daily.temperature_2m_min.forEach((mintempvalue, index) => {
         const mintemp = document.querySelectorAll('.mintemp');
