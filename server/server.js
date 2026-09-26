d30fe4317dbbbb53a0c000f86559104ec44faca2
Rohil Masssey <rohilmasssey@gmail.com>
2026-09-26 20:28:56 +0530

    Update javascript

--- server/server.js
+++ server/server.js
@@ -17,12 +17,12 @@ app.get('/geo',async(req, res) => {
     const {lat, lon} = req.query;
 
     try{
-        // const currentResponse = await fetch(
-        //     // `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.weatherapikey}&units=metric`
-        //     `https://geocoding-api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}`
-        // ); 
+        const currentResponse = await fetch(
+            // `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.weatherapikey}&units=metric`
+            `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code&hourly=temperature_2m,weather_code,relative_humidity_2m&daily=weather_code,temperature_2m_max,temperature_2m_min&forecast_days=7`
+        ); 
 
-        // const currentData = await currentResponse.json();
+        const currentData = await currentResponse.json();
         const forecastResponse = await fetch(
             `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code&hourly=temperature_2m,weather_code,relative_humidity_2m&daily=weather_code,temperature_2m_max,temperature_2m_min&forecast_days=7`
         )
@@ -31,7 +31,7 @@ app.get('/geo',async(req, res) => {
 
         console.log(forecastData);
         res.json({
-            // current:currentData,
+            current:currentData,
             forecast:forecastData
         });
     }catch(error){
